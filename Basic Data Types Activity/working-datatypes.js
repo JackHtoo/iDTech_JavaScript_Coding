79f221ba9769134d61738b46d2a6f229e6e5cb74
JackHtoo <108778457+JackHtoo@users.noreply.github.com>
2022-07-07 10:47:37 -0700

    commit

---
+++ Basic Data Types Activity/working-datatypes.js
@@ -0,0 +1,21 @@
+
+let greetings = "Welcome to the World of Cats";
+let name = "Waldy";
+let title = "The Chaser of the Red Dot";
+let worldName = "House of Giants";
+
+let age = 7;
+let year = 2022;
+let centruy = 21;
+
+let arsenal = ["Paw Strike", "Tailwhip", "Pounce", "Menacing Stare"];
+
+let alwaysLandOnPaws = true;
+
+console.log(greetings + "My name is " + name + '.');
+console.log("I'm the " + title + '.');
+console.log("As the official " + title + ", I have been chasing for years in " + worldName + '.');
+console.log("I am " + age + " years old.");
+console.log("The year is " + year + " and it is the " + centruy + "st.");
+console.log("I tear down buildings with my " + arsenal[0] + " and I " + arsenal[2] + " at the gaints living amoung me.");
+console.log("And it is always " + alwaysLandOnPaws + " that I land on my feat.");
